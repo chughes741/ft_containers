a7d6679e42e68b64b245c9ddb65a06d030018ad1
Cole Hughes <coltenhughes@gmail.com>
2022-12-29 18:41:44 -0500

    Added another test for ft::pair

--- src/test/utility_test.cpp
+++ src/test/utility_test.cpp
@@ -8,10 +8,14 @@
 
 TEST(PairTest, BasicAssertions) {
   ft::pair<bool, bool> ft_val_1 = ft::make_pair(true, true);
-  ft::pair<bool, bool> ft_val_2 = ft::make_pair(true, true);
   std::pair<bool, bool> std_val_1 = std::make_pair(true, true);
-  std::pair<bool, bool> std_val_2 = std::make_pair(true, true);
 
   EXPECT_EQ(ft_val_1.first, std_val_1.first);
   EXPECT_EQ(ft_val_1.second, std_val_1.second);
+
+  ft::pair<int, bool> ft_val_2 = ft::make_pair(42, false);
+  std::pair<int, bool> std_val_2 = std::make_pair(24, true);
+
+  EXPECT_NE(ft_val_2.first, std_val_2.first);
+  EXPECT_NE(ft_val_2.second, std_val_2.second);
 }
