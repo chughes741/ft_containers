95438716616119d762876ca24ce57486e2db840c
Cole Hughes <coltenhughes@gmail.com>
2022-12-29 22:27:54 -0500

    Split pair tests up

--- src/test/utility_test.cpp
+++ src/test/utility_test.cpp
@@ -6,39 +6,38 @@
 #include <iostream>
 #include <utility>
 
-TEST(PairTest, BasicAssertions) {
-  {  // Default constructor and make pair
-    ft::pair<bool, bool> ft_val;
-    ft_val = ft::make_pair(true, true);
-    std::pair<bool, bool> std_val;
-    std_val = std::make_pair(true, true);
+TEST(PairTest, ConstructorsAndMakePair) {
+  ft::pair<bool, bool> ft_val_1;
+  ft_val_1 = ft::make_pair(true, true);
+  std::pair<bool, bool> std_val_1;
+  std_val_1 = std::make_pair(true, true);
 
-    EXPECT_EQ(ft_val.first, std_val.first);
-    EXPECT_EQ(ft_val.second, std_val.second);
-  }
-  {  // Default constructor overload with init values
-    ft::pair<int, bool>  ft_val(42, false);
-    std::pair<int, bool> std_val(24, true);
+  EXPECT_EQ(ft_val_1.first, std_val_1.first);
+  EXPECT_EQ(ft_val_1.second, std_val_1.second);
 
-    EXPECT_NE(ft_val.first, std_val.first);
-    EXPECT_NE(ft_val.second, std_val.second);
-  }
-  {  // Copy constructor and copy assignment
-    ft::pair<int, bool> ft_val_1 = ft::make_pair(1, true);
-    ft::pair<int, bool> ft_val_2(ft_val_1);
-    ft::pair<int, bool> ft_val_3 = ft_val_2;
+  ft::pair<int, bool>  ft_val_2(42, false);
+  std::pair<int, bool> std_val_2(24, true);
 
-    EXPECT_EQ(ft_val_2.first, ft_val_2.first);
-    EXPECT_EQ(ft_val_3.second, ft_val_3.second);
-  }
-  {  // Comparison tests
-    ft::pair<int, int> ft_val_1(24, 24);
-    EXPECT_TRUE(ft_val_1 == ft_val_1);
+  EXPECT_NE(ft_val_2.first, std_val_2.first);
+  EXPECT_NE(ft_val_2.second, std_val_2.second);
+}
 
-    ft::pair<int, int> ft_val_2(42, 24);
-    EXPECT_TRUE(ft_val_1 < ft_val_2);
+TEST(PairTest, CopyConstructorAndAssignment) {
+  ft::pair<int, bool> ft_val_1 = ft::make_pair(1, true);
+  ft::pair<int, bool> ft_val_2(ft_val_1);
+  ft::pair<int, bool> ft_val_3 = ft_val_2;
 
-    ft::pair<int, int> ft_val_3(42, 42);
-    EXPECT_TRUE(ft_val_2 < ft_val_3);
-  }
+  EXPECT_EQ(ft_val_2.first, ft_val_2.first);
+  EXPECT_EQ(ft_val_3.second, ft_val_3.second);
+}
+
+TEST(PairTest, Comparisons) {
+  ft::pair<int, int> ft_val_1(24, 24);
+  EXPECT_TRUE(ft_val_1 == ft_val_1);
+
+  ft::pair<int, int> ft_val_2(42, 24);
+  EXPECT_TRUE(ft_val_1 < ft_val_2);
+
+  ft::pair<int, int> ft_val_3(42, 42);
+  EXPECT_TRUE(ft_val_2 < ft_val_3);
 }
