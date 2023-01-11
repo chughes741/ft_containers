dde5312d105e12782b9c54589dfadd1a2f880252
Cole Hughes - 42 <coltenhughes@gmail.com>
2023-01-11 15:23:05 -0500

    Added code for equal and lex_comp

--- include/algorithm.hpp
+++ include/algorithm.hpp
@@ -8,20 +8,54 @@
 namespace ft {
 
 template <class InputIter1, class InputIter2>
-bool equal(InputIter1 first1, InputIter1 last1, InputIter2 first2){};
+bool equal(InputIter1 first1, InputIter1 last1, InputIter2 first2) {
+  for (; first1 != last1; ++first1, ++first2) {
+    if (!(*first1 == *first2)) {
+      return false;
+    }
+  }
+  return true;
+};
 
 template <class InputIter1, class InputIter2, class BinaryPredicate>
 bool equal(InputIter1 first1, InputIter1 last1, InputIter2 first2,
-           BinaryPredicate p){};
+           BinaryPredicate p) {
+  for (; first1 != last1; ++first1, ++first2) {
+    if (!p(*first1, *first2)) {
+      return false;
+    }
+  }
+  return true;
+};
 
 template <class InputIter1, class InputIter2>
 bool lexicographical_compare(InputIter1 first1, InputIter1 last1,
-                             InputIter2 first2, InputIter2 last2){};
+                             InputIter2 first2, InputIter2 last2) {
+  for (; first1 != last1 && first2 != last2; ++first1, ++first2) {
+    if (*first1 < *first2) {
+      return true;
+    }
+    if (*first2 < *first1) {
+      return false;
+    }
+  }
+  return first1 == last1 && first2 != last2;
+};
 
 template <class InputIter1, class InputIter2, class Compare>
 bool lexicographical_compare(InputIter1 first1, InputIter2 last1,
                              InputIter2 first2, InputIter2 last2,
-                             Compare comp){};
+                             Compare comp) {
+  for (; first1 != last1 && first2 != last2; ++first1, ++first2) {
+    if (comp(*first1, *first2)) {
+      return true;
+    }
+    if (comp(*first2, *first1)) {
+      return false;
+    }
+  }
+  return first1 == last1 && first2 != last2;
+};
 
 }  // namespace ft
 
