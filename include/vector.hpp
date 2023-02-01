36e6334149c965f74ed9f1499bc13f641f7b58ed
Cole Hughes <coltenhughes@gmail.com>
2023-02-01 14:12:07 -0500

    Added fn comments to vector

--- include/vector.hpp
+++ include/vector.hpp
@@ -29,10 +29,16 @@ class vector_const_iterator {
 
   const_pointer ptr_;
 
-  vector_const_iterator() ft_noexcept : ptr_() {}
-  vector_const_iterator(pointer ptr) ft_noexcept : ptr_(ptr) {}
-  const value_type&      operator*() const ft_noexcept { return *ptr_; }
-  const_pointer          operator->() const ft_noexcept { return ptr_; }
+  vector_const_iterator() ft_noexcept : ptr_() {
+  }
+  vector_const_iterator(pointer ptr) ft_noexcept : ptr_(ptr) {
+  }
+  const value_type& operator*() const ft_noexcept {
+    return *ptr_;
+  }
+  const_pointer operator->() const ft_noexcept {
+    return ptr_;
+  }
   vector_const_iterator& operator++() ft_noexcept {
     ++ptr_;
     return *this;
@@ -131,10 +137,16 @@ class vector_iterator {  //: public ft::vector_const_iterator<MyVector> {
 
   pointer ptr_;
 
-  vector_iterator() ft_noexcept : ptr_() {}
-  vector_iterator(pointer ptr) ft_noexcept : ptr_(ptr) {}
-  value_type&      operator*() const ft_noexcept { return *ptr_; }
-  const_pointer    operator->() const ft_noexcept { return ptr_; }
+  vector_iterator() ft_noexcept : ptr_() {
+  }
+  vector_iterator(pointer ptr) ft_noexcept : ptr_(ptr) {
+  }
+  value_type& operator*() const ft_noexcept {
+    return *ptr_;
+  }
+  const_pointer operator->() const ft_noexcept {
+    return ptr_;
+  }
   vector_iterator& operator++() ft_noexcept {
     ++ptr_;
     return *this;
@@ -220,6 +232,7 @@ bool operator>=(const ft::vector_iterator<MyVector>& lhs,
   return !(lhs < rhs);
 }
 
+// Vector<T> class template
 template <class T, class Allocator = std::allocator<T>>
 class vector {
  public:
@@ -245,17 +258,22 @@ class vector {
   pointer        end_   = NULL;
 
  public:
+  // Default constructor
   vector() ft_noexcept : alloc_(Allocator()),
                          first_(alloc_.allocate(0)),
                          last_(first_),
-                         end_(first_) {}
+                         end_(first_) {
+  }
 
+  // Constructor with allocator override
   explicit vector(const Allocator& alloc) ft_noexcept
       : alloc_(alloc),
         first_(alloc_.allocate(0)),
         last_(first_),
-        end_(first_) {}
+        end_(first_) {
+  }
 
+  // Constructor with size and default value
   explicit vector(size_type count, const value_type& value = value_type(),
                   const Allocator& alloc = Allocator())
       : alloc_(alloc),
@@ -267,6 +285,7 @@ class vector {
     }
   }
 
+  // Range constructor
   template <class InputIt>
   vector(InputIt first, InputIt last, const Allocator& alloc = Allocator())
       : alloc_(alloc) {
@@ -274,109 +293,291 @@ class vector {
     last_  = first_ + (last - first);
     std::copy(first, last, iterator(first_));
   }
+
+  // Copy constructor
   vector(const vector& other) : alloc_(other.alloc_) {
     *this = vector(other.begin(), other.end());
   }
+
+  // Copy assignment operator overload
   vector& operator=(const vector& rhs) {
     if (*this == rhs) {
       return *this;
     }
     return *this;
   }
+
+  // Destructor
   ~vector() ft_noexcept {
     for (size_type i = 0; i < this->size(); ++i) {
       alloc_.destroy(first_ + i);
     }
     alloc_.deallocate(first_, last_ - first_);
   }
+
+  // Replaces the contents with count copies of value value
   void assign(size_type count, const value_type& value) {
     this->resize(count);
     std::fill(this->begin(), this->end(), value);
   }
+
+  // Replaces the contents with copies of those in the range [first, last)
   template <class InputIt>
   void assign(InputIt first, InputIt last) {
     this->resize((first - last) / sizeof(size_type));
     std::copy(first, last, this->begin());
   }
-  allocator_type get_allocator() const ft_noexcept { return alloc_; }
 
+  // Returns copy of the allocator<T>
+  allocator_type get_allocator() const ft_noexcept {
+    return alloc_;
+  }
+
+  // Returns value of the element at(index), checks range
   value_type& at(size_type position) {
     RangeCheck(position);
     return *(first_ + position);
   }
+
+  // Returns value of the element at(index), checks range
   const value_type& at(size_type position) const {
     RangeCheck(position);
     return *(first_ + position);
   }
+
+  // Returns value of the element at [index], does not check range
   value_type& operator[](size_type position) {
     return *(this->first_ + position);
   }
+
+  // Returns value of the element at [index], does not check range
   const value_type& operator[](size_type position) const {
     return (*this)[position];
   }
-  value_type&       front() { return *(this->first_); }
-  const value_type& front() const { return *(this->first_); }
-  value_type&       back() { return *(this->last_); }
-  const value_type& back() const { return *(this->last_); }
-  value_type*       data() ft_noexcept { return this->first_; }
-  const value_type* data() const ft_noexcept { return &(this->first_); }
 
-  iterator         begin() ft_noexcept { return iterator(first_); }
-  const_iterator   begin() const ft_noexcept { return const_iterator(first_); }
-  iterator         end() ft_noexcept { return iterator(last_ + 1); }
-  const_iterator   end() const ft_noexcept { return const_iterator(last_ + 1); }
+  // Returns value of the first element in the array
+  value_type& front() {
+    return *(this->first_);
+  }
+
+  // Returns value of the first element in the array
+  const value_type& front() const {
+    return *(this->first_);
+  }
+
+  // Returns value of the last element in the array
+  value_type& back() {
+    return *(this->last_);
+  }
+
+  // Returns value of the last element in the array
+  const value_type& back() const {
+    return *(this->last_);
+  }
+
+  // Returns a raw pointer to the start of the underlying array
+  value_type* data() ft_noexcept {
+    return this->first_;
+  }
+
+  // Returns a raw pointer to the start of the underlying array
+  const value_type* data() const ft_noexcept {
+    return &(this->first_);
+  }
+
+  // Returns a iterator to the start of the vector
+  iterator begin() ft_noexcept {
+    return iterator(first_);
+  }
+
+  // Returns a iterator to the start of the vector
+  const_iterator begin() const ft_noexcept {
+    return const_iterator(first_);
+  }
+
+  // Returns a iterator to the end of the vector
+  iterator end() ft_noexcept {
+    return iterator(last_ + 1);
+  }
+
+  // Returns a iterator to the end of the vector
+  const_iterator end() const ft_noexcept {
+    return const_iterator(last_ + 1);
+  }
+
+  // Returns a reverse iterator to the start of the reversed vector
   reverse_iterator rbegin() ft_noexcept {
     return reverse_iterator(this->last_, this);
   }
+
+  // Returns a reverse iterator to the start of the reversed vector
   const_reverse_iterator rbegin() const ft_noexcept {
     return const_reverse_iterator(this->last_, this);
   }
+
+  // Returns a reverse iterator to the end of the reversed vector
   reverse_iterator rend() ft_noexcept {
     return reverse_iterator(this->first_ - 1, this);
   }
+
+  // Returns a reverse iterator to the end of the reversed vector
   const_reverse_iterator rend() const ft_noexcept {
     return const_reverse_iterator(this->first_ - 1, this);
   }
 
-  bool empty() const ft_noexcept { return this->size() == 0 ? true : false; }
+  // Returns true if there are no elements in use
+  bool empty() const ft_noexcept {
+    return this->size() == 0 ? true : false;
+  }
+
+  // Returns the number of elements currently in use
   size_type size() const ft_noexcept {
     return (this->last_ - this->first_);
   }
-  size_type max_size() const ft_noexcept { return alloc_.max_size(); }
-  void      reserve(size_type new_cap) {
-         if (new_cap > this->size()) {
-          //  pointer new_first_ = alloc_.allocate(new_cap, this->first_);
-          //  std::copy(this->begin(), this->end(), iterator(new_first_, this));
 
-          //  this->last_ = new_first_ + this->size;
-          //  this->end_  = new_first_ + (new_cap * sizeof(size_type));
-          //  alloc_.deallocate(this->first_);
-          //  this->first_ = new_first_;
+  // Returns the max allowable size of the array
+  size_type max_size() const ft_noexcept {
+    return alloc_.max_size();
+  }
+
+  // Increases the reserved capacity of the array
+  void reserve(size_type new_cap) {
+    if (new_cap > this->size()) {
+      //  pointer new_first_ = alloc_.allocate(new_cap, this->first_);
+      //  std::copy(this->begin(), this->end(), iterator(new_first_, this));
+
+      //  this->last_ = new_first_ + this->size;
+      //  this->end_  = new_first_ + (new_cap * sizeof(size_type));
+      //  alloc_.deallocate(this->first_);
+      //  this->first_ = new_first_;
     }
   }
-  size_type capacity() const { return end_ - first_; }
 
-  void     clear() ft_noexcept {}
-  iterator insert(const_iterator pos_, const value_type& value_) {}
-  iterator insert(const_iterator pos_, size_type count_,
-                  const value_type& value_) {}
+  // Returns the number of elements that have space allocated for them
+  size_type capacity() const {
+    return end_ - first_;
+  }
+
+  // Erases all elements from the container. After this call, size() is zero.
+  void clear() ft_noexcept {
+    Reallocate(0);
+  }
+
+  // Inserts value before pos
+  iterator insert(const_iterator pos, const value_type& value) {
+  }
+
+  // Inserts count copies of the value before pos
+  iterator insert(const_iterator pos, size_type count,
+                  const value_type& value) {
+  }
+
+  // Inserts elements from range [first, last) before pos
   template <class InputIt>
-  iterator insert(const_iterator pos_, InputIt first_, InputIt last_) {}
-  iterator erase(iterator pos_) {}
-  iterator erase(iterator first_, iterator last_) {}
-  void     push_back(const value_type& value_) {}
-  void     pop_back() {}
-  void     resize(size_type count_, value_type value_ = value_type()) {}
-  void     swap(vector& other) {}
+  iterator insert(const_iterator pos, InputIt first, InputIt last) {
+  }
 
+  // Removes the element at pos
+  iterator erase(iterator pos) {
+    alloc_.destroy(pos);
+    alloc_.construct(pos);
+    for (iterator it = pos; it + 1 != last_; ++it) {
+      *it = *(it + 1);
+    }
+    alloc_.destroy(last_);
+    --last_;
+    return pos + 1;
+  }
+
+  //  Removes the elements in the range [first, last)
+  iterator erase(iterator first, iterator last) {
+    for (iterator it = first; first != last; ++it) {
+      erase(it);
+    }
+    return first + 1;
+  }
+
+  // Appends the given element value to the end of the container
+  void push_back(const value_type& value) {
+    Reallocate(SmartSize(this->size() + 1));
+    *last_ = value;
+  }
+
+  // Removes the last element of the container
+  void pop_back() {
+    Reallocate(SmartSize(this->size() - 1));
+  }
+
+  // Resizes the container to contain count elements
+  void resize(size_type count, value_type value = value_type()) {
+    Reallocate(count, value);
+  }
+
+  // Swaps underlying array between two vectors
+  void swap(vector& other) {
+    pointer temp_first_ = first_;
+    pointer temp_last_  = last_;
+    pointer temp_end_   = end_;
+
+    first_ = other.first_;
+    last_  = other.last_;
+    end_   = other.end_;
+
+    other.first_ = temp_first_;
+    other.last_  = temp_last_;
+    other.end_   = temp_end_;
+  }
+
+  // Helper functions
  private:
+  // Returns next 2^n larger than size
+  size_type SmartSize(size_type size) {
+    size_type smart_size = size == 0 ? 0 : 1;
+    while (smart_size < size && smart_size < (this->max_size() / 2)) {
+      smart_size *= 2;
+    }
+    if (smart_size < size) smart_size = this->max_size();
+    return smart_size;
+  };
+
+  // Reallocated the vectors array
+  void Reallocate(size_type size, value_type value = value_type()) {
+    pointer new_first_ = alloc_.allocate(size, first_);
+    pointer new_last_ =
+        new_first_ + (this->size() < size ? this->size() : size);
+    pointer new_end_ = new_first_ + size;
+
+    for (size_type i = 0; i < size; ++i) {
+      alloc_.construct(new_first_ + i, value);
+    }
+
+    for (size_type i = 0; i < size && i < this->size(); ++i) {
+      new_first_[i] = first_[i];
+    }
+
+    for (size_type i = 0; i < this->size(); ++i) {
+      alloc_.destroy(&first_[i]);
+    }
+
+    alloc_.deallocate(first_, end_ - first_);
+    first_ = new_first_;
+    last_  = new_last_;
+    end_   = new_end_;
+  }
+
+  // Checks if new_size will fit in currently allocated memory
+  bool CheckSize(size_type new_size) {
+    return !(new_size > end_ - first_);
+  }
+
+  // Checks if pos is iside the array, throws std::out_of_range
   void RangeCheck(size_type pos) {
     if (pos > this->size()) {
-      std::string message = " pos (piece of shit)";
-      // message.append(pos);
-      // message.append(") >= this->size() (which is ");
-      // message.append(this->size());
-      // message.append("\n");
+      std::string message = " pos (";
+      message.append(std::to_string(pos));
+      message.append(") >= this->size() (which is ");
+      message.append(std::to_string(this->size()));
+      message.append(")\n");
       throw std::out_of_range(message);
     }
   }
@@ -423,7 +624,8 @@ bool operator>=(const ft::vector<T, Alloc>& lhs,
 }
 
 template <class T, class Alloc>
-void swap(ft::vector<T, Alloc>& lhs, ft::vector<T, Alloc>& rhs) {}
+void swap(ft::vector<T, Alloc>& lhs, ft::vector<T, Alloc>& rhs) {
+}
 
 }  // namespace ft
 
