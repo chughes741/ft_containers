be7edf80067aa0dda9cad4c4344bd87b4a085201
Cole Hughes <coltenhughes@gmail.com>
2023-02-02 22:13:30 -0500

    Updated vector reference typedefs

--- include/vector.hpp
+++ include/vector.hpp
@@ -190,8 +190,8 @@ class vector {
   typedef T                                        value_type;
   typedef typename allocator_type::pointer         pointer;
   typedef typename allocator_type::const_pointer   const_pointer;
-  typedef typename allocator_type::reference       reference;
-  typedef typename allocator_type::const_reference const_reference;
+  typedef value_type&                              reference;
+  typedef const value_type&                        const_reference;
   typedef typename allocator_type::size_type       size_type;
   typedef typename allocator_type::difference_type difference_type;
 
