c6e5acb1fb536e687aae2a48ad00e9adc5f6784d
Cole Hughes - 42 <coltenhughes@gmail.com>
2023-02-06 12:49:58 -0500

    Fixed typo in lexico cmp

--- include/algorithm.hpp
+++ include/algorithm.hpp
@@ -43,7 +43,7 @@ bool lexicographical_compare(InputIter1 first1, InputIter1 last1,
 };
 
 template <class InputIter1, class InputIter2, class Compare>
-bool lexicographical_compare(InputIter1 first1, InputIter2 last1,
+bool lexicographical_compare(InputIter1 first1, InputIter1 last1,
                              InputIter2 first2, InputIter2 last2,
                              Compare comp) {
   for (; first1 != last1 && first2 != last2; ++first1, ++first2) {
