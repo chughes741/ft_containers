ed6d7c8a614519ec6c0cce15ebfff563699f8912
Cole Hughes <coltenhughes@gmail.com>
2023-02-07 18:57:45 -0500

    Added partial tree implementation

--- include/tree.hpp
+++ include/tree.hpp
@@ -8,12 +8,11 @@
 
 #include "iterator.hpp"
 #include "type_traits.hpp"
-// #include "utility.hpp"
+#include "utility.hpp"
 
 #define ft_noexcept throw()
 
-using ft::enable_if;
-using ft::is_integral;
+using ft::pair;
 using std::bidirectional_iterator_tag;
 
 namespace ft {
@@ -23,17 +22,17 @@ class tree_const_iterator {
  public:
   typedef bidirectional_iterator_tag iterator_category;
 
-  typedef typename MyTree::node_ptr        node_ptr;
+  typedef typename MyTree::node_pointer    node_pointer;
   typedef typename MyTree::value_type      value_type;
   typedef typename MyTree::difference_type difference_type;
   typedef typename MyTree::pointer         pointer;
   typedef const value_type&                reference;
 
-  node_ptr ptr_;
+  node_pointer ptr_;
 
   tree_const_iterator() ft_noexcept : ptr_() {
   }
-  tree_const_iterator(node_ptr n_ptr) ft_noexcept : ptr_(n_ptr) {
+  tree_const_iterator(node_pointer n_ptr) ft_noexcept : ptr_(n_ptr) {
   }
   reference operator*() const ft_noexcept {
     return *ptr_;
@@ -47,7 +46,7 @@ class tree_const_iterator {
   }
   tree_const_iterator operator++(int) ft_noexcept {
     tree_const_iterator temp = *this;
-    ++ptr;
+    ++ptr_;
     return temp;
   }
   tree_const_iterator operator--() ft_noexcept {
@@ -70,16 +69,17 @@ class tree_const_iterator {
 template <class MyTree>
 class tree_iterator : public tree_const_iterator<MyTree> {
  public:
-  typedef tree_const_iterator<MyTree>      base_iterator;
-  typedef bidirectional_iterator_tag       iterator_category;
-  typedef typename MyTree::value_type      value_type;
-  typedef typename MyTree::difference_type difference_type;
-  typedef typename MyTree::pointer         pointer;
-  typedef value_type&                      reference;
+  typedef tree_const_iterator<MyTree>               base_iterator;
+  typedef typename base_iterator::iterator_category iterator_category;
+  typedef typename base_iterator::value_type        value_type;
+  typedef typename base_iterator::difference_type   difference_type;
+  typedef typename base_iterator::pointer           pointer;
+  typedef typename base_iterator::value_type&       reference;
+  typedef typename base_iterator::node_pointer      node_pointer;
 
   tree_iterator() ft_noexcept : base_iterator() {
   }
-  tree_iterator(n_ptr) ft_noexcept : base_iterator(n_ptr) {
+  tree_iterator(node_pointer n_ptr) ft_noexcept : base_iterator(n_ptr) {
   }
   reference operator*() const ft_noexcept {
     return const_cast<reference>(base_iterator::operator*());
@@ -107,104 +107,91 @@ class tree_iterator : public tree_const_iterator<MyTree> {
   }
 };
 
-template <class value_type_, class void_ptr_>
+template <typename T>
 struct tree_node {
-  typedef tree_node*  node_ptr;
-  typedef value_type_ value_type;
+  typedef T          value_type;
+  typedef tree_node* node_pointer;
 
-  node_ptr   parent_;
-  node_ptr   left_;
-  node_ptr   right_;
-  value_type value_;
-  // char color_; // red_ or black_, black_ if head
-  // bool nil_; // true only if head node
+  node_pointer parent_;
+  node_pointer left_;
+  node_pointer right_;
+  value_type   value_;
 
-  // enum red_black_ {
-  // red_,
-  // black_
-  // };
-
-  tree_node() ft_noexcept {
+  tree_node() ft_noexcept : parent_(NULL),
+                            left_(NULL),
+                            right_(NULL),
+                            value_(NULL) {
   }
-  tree_node(const tree_node& other) ft_noexcept {
-  }
-  tree_node& operator=(const tree_node& rhs) ft_noexcept {
-  }
-  template <class Allocator>
-  static node_ptr buyHeadNode(Allocator& alloc);
-  template <class Allocator>
-  static node_ptr buyNode(Allocator& alloc, node_ptr my_head,
-                          value_type& value);
-  template <class Allocator>
-  static void freeNode(Allocator& alloc, node_ptr node) ft_noexcept;
 };
 
-template <class value_types_>
-class tree_value {  // TODO public: container_base
- public:
-  typedef typename value_types_::node_ptr node_ptr;
-
-  typedef typename value_types_::value_type      value_type;
-  typedef typename value_types_::size_type       size_type;
-  typedef typename value_types_::difference_type difference_type;
-  typedef typename value_types_::pointer         pointer;
-  typedef typename value_types_::const_pointer   const_pointer;
-  typedef value_type&                            reference;
-  typedef const value_type&                      const_reference;
-
-  typedef tree_const_iterator<tree_value> const_iterator;
-
-  // enum red_black_ {
-  // red_,
-  // black_
-  // };
-
- private:
-  node_ptr  head_;
-  size_type size_;
-
- public:
-  tree_value() ft_noexcept : head_(), size_(0) {
-  }
-  // Returns rightmost node in subtree node
-  static node_ptr max_(node_ptr node) ft_noexcept {
-  }
-  // Returns leftmost node in subtree node
-  static node_ptr min_(node_ptr node) ft_noexcept {
-  }
-  // Promotes right node to root of subtree
-  void leftRotate(node_ptr node) ft_noexcept {
-  }
-  // Promotes left node to root of subtree
-  void rightRotate(node_ptr node) ft_noexcept {
-  }
-  //
-  node_ptr extract(const_iterator where) ft_noexcept {
-  }
-  //
-  node_ptr insert(node_ptr location, node_ptr new_node) ft_noexcept {
-  }
-  // 
-  template <class AlNode>
-  void eraseTree(AlNode &alloc, node_ptr root) ft_noexcept {}
-  //
-  template <class AlNode>
-  void eraseHead(AlNode &alloc) ft_noexcept {}
-};
-
-template <class Key, class T, class Compare = std::less<Key>,
-          class Allocator = std::allocator<T>>
+template <class T, class Compare, class Allocator>
 class tree {
  public:
-  tree();
-  explicit tree(const Compare&        comp,
-                const allocator_type& alloc = allocator_type());
-  template <class InputIt>
-  tree(InputIt first, InputIt last, const Compare& comp = Compare(),
-       const allocator_type& alloc = allocator_type());
-  tree(const tree& other);
-  tree& operator=(const tree& other);
-  ~tree();
+  typedef T                                        value_type;
+  typedef Compare                                  value_compare;
+  typedef Allocator                                allocator_type;
+  typedef typename allocator_type::size_type       size_type;
+  typedef typename allocator_type::difference_type difference_type;
+  typedef value_type&                              reference;
+  typedef const value_type&                        const_reference;
+  typedef typename allocator_type::pointer         pointer;
+  typedef typename allocator_type::const_pointer   const_pointer;
+  typedef tree_iterator<value_type>                iterator;
+  typedef tree_const_iterator<value_type>          const_iterator;
+
+ private:
+  typedef std::allocator<tree_node<T>>        node_allocator;
+  typedef typename tree_node<T>::node_pointer node_pointer;
+
+ public:
+  allocator_type value_alloc_;
+  node_allocator node_alloc_;
+  value_compare  compare_;
+  node_pointer   root_;
+  size_type      size_;
+
+  // Default constructor
+  tree()
+      : value_alloc_(allocator_type()),
+        node_alloc_(node_allocator()),
+        compare_(value_compare()),
+        root_(NULL),
+        size_(0) {
+  }
+
+  // Destructor
+  ~tree() {
+  }
+
+  // Inserts a new node into the tree
+  void insert(const value_type& value) {
+    node_pointer position = find_insert_position(value);
+  }
+
+  // Returns parent node of value
+  node_pointer find_insert_position(const value_type& value) {
+    node_pointer node = root_;
+    while (node) {
+      // value < node->value
+      if (value_comp()(value, node->value)) {
+        if (node->left_ == NULL) return node;
+        node = node->left_;
+      } else {
+        if (node->right_ == NULL) return node;
+        node = node->right_;
+      }
+    }
+  }
+
+  //
+  value_compare& value_comp() {
+    return compare_;
+  }
+
+  //
+  const value_compare& value_comp() const {
+    return compare_;
+  }
 };
 
 }  // namespace ft
